5b527b3900e4f6799bed179f60aab33ec661daea
Skye Book <skye.book@gmail.com>
2011-09-26 19:21:10 -0400

    Added license header, updated box to match camera's aspect ratio

--- src/net/skyebook/jme3android/camera/Game.java
+++ src/net/skyebook/jme3android/camera/Game.java
@@ -1,5 +1,19 @@
-/**
- * 
+/**  
+ *  jME3 Android Camera - A demo for integrating the Android camera with jMonkeyEngine
+ *  Copyright (C) 2011 Skye Book
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package net.skyebook.jme3android.camera;
 
@@ -44,7 +58,7 @@ public class Game extends SimpleApplication {
 	 */
 	@Override
 	public void simpleInitApp() {
-		box = new Box(Vector3f.ZERO, 1, 1, 1);
+		box = new Box(Vector3f.ZERO, 3, 2, 1);
 		Geometry geometry = new Geometry("box", box);
 		
 		material = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
